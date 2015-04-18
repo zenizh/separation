d914a13bf13353761702bc12e6598dceacb5272c
kami <hiroki.zenigami@gmail.com>
2015-04-19 07:09:41 +0900

    Modify railtie

--- lib/variauto/railtie.rb
+++ lib/variauto/railtie.rb
@@ -2,7 +2,7 @@ module Variauto
   class Railtie < Rails::Railtie
     initializer 'variauto' do
       ActiveSupport.on_load :action_controller do
-        ApplicationController.send :include, Variauto::Base
+        ActionController::Base.send :include, Variauto::Base
       end
     end
   end
